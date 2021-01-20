37f939821f288da36573d86ee28c6a23590fab38
yemuc <yemuc98@gmail.com>
2021-01-20 13:29:16 +0800

    升级Mybatis-plus后分页改用MybatisPlusInterceptor

--- mall-portal/src/main/java/com/yemu/mallportal/config/MybatisPlusConfig.java
+++ mall-portal/src/main/java/com/yemu/mallportal/config/MybatisPlusConfig.java
@@ -1,6 +1,6 @@
 package com.yemu.mallportal.config;
 
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -13,7 +13,7 @@ public class MybatisPlusConfig {
      * 分页配置
      */
     @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        return new PaginationInterceptor();
+    public MybatisPlusInterceptor paginationInterceptor() {
+        return new MybatisPlusInterceptor();
     }
 }
